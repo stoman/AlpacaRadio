738155dae3b0cd354203e7d718436a541bdc2bd1
Stefan Toman <git@stoman.de>
2020-12-27 14:39:22 +0100

    Import Spring Initializr project

---
+++ src/main/kotlin/de/stoman/alpacaradio/AlpacaRadioApplication.kt
@@ -0,0 +1,11 @@
+package de.stoman.alpacaradio
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AlpacaRadioApplication
+
+fun main(args: Array<String>) {
+	runApplication<AlpacaRadioApplication>(*args)
+}
