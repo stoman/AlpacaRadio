9421a3e768b55cad1c744b3296eeddb92671728e
Stefan Toman <git@stoman.de>
2020-12-28 23:09:14 +0100

    Tune scores
    Also add debug logging of scores.

--- src/main/kotlin/de/stoman/alpacaradio/services/HistoryService.kt
+++ src/main/kotlin/de/stoman/alpacaradio/services/HistoryService.kt
@@ -4,6 +4,8 @@ import de.stoman.alpacaradio.model.History
 import de.stoman.alpacaradio.model.HistoryRepository
 import de.stoman.alpacaradio.model.Video
 import de.stoman.alpacaradio.model.VideoRepository
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import java.time.Clock
@@ -18,19 +20,21 @@ class HistoryService(
   @Autowired private val historyRepository: HistoryRepository,
   @Autowired private val videoRepository: VideoRepository,
   private val clock: Clock = Clock.systemUTC(),
+  private val logger: Logger = LoggerFactory.getLogger(HistoryService::class.java)
 ) {
   private fun scoreVideo(video: Video): Double {
     // Score between 0 and 1. The higher `x`, the higher the score. The biggest increase is around `mean`.
     fun partialScore(x: Double, mean: Double = 0.0, scale: Double = 1.0): Double = 0.5 + 0.5 * tanh((x - mean) / scale)
 
-    var score = 0.0
+    var score = 0.01
     score += 0.5 * (Video.LAST_PLAYED_SIZE - video.lastPlayed.size)
     if (video.lastPlayed.isEmpty()) {
       score += 10.0
     }
     score += video.lastPlayed.map { Duration.between(it, clock.instant()).toMinutes() }
-      .map { partialScore(it.toDouble(), mean = 3 * 60.0) }.sum()
+      .mapIndexed { i, minutes -> partialScore(minutes.toDouble(), mean = (i + 1) * 3 * 60.0, scale = 60.0) }.sum()
 
+    logger.debug("scoreVideo", "${video.title}: $score")
     return score
   }
 
